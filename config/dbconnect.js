f2030b480d4a3a1d4974e92793d00bc30d6a9a05
sasineelakandan <devasasi887@gmail.com>
2024-04-11 14:10:27 +0530

    testing commit

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,7 @@
 const mongoose=require('mongoose')
 module.exports=mongoose.connect('mongodb+srv://devasasi:WxAGvVRGeMH8lV82@cluster0.elv16wi.mongodb.net/bigbag')
 .then(()=>{
-console.log('mongodb atlas connected')
+console.log('mongodb connected')
 })
 .catch((error)=>{
  console.log(error)
