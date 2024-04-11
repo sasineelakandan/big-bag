b4d315d604245b81d5b436923e7328b5f8393f06
sasineelakandan <devasasi887@gmail.com>
2024-04-11 14:08:56 +0530

    jdfhadskj
    Signed-off-by: sasineelakandan <devasasi887@gmail.com>

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,7 @@
 const mongoose=require('mongoose')
 module.exports=mongoose.connect('mongodb+srv://devasasi:WxAGvVRGeMH8lV82@cluster0.elv16wi.mongodb.net/bigbag')
 .then(()=>{
-console.log('mongodb connected')
+console.log('mongodb atlas connected')
 })
 .catch((error)=>{
  console.log(error)
