d03e242c173ee7e4d2c16bfa51e19533278baacb
sasineelakandan <devasasi887@gmail.com>
2024-10-27 13:39:51 +0530

    finish cicd pipeline

--- middlewere/googleAuth.js
+++ middlewere/googleAuth.js
@@ -2,30 +2,30 @@
 
 
 
-const passport=require('passport')
+// const passport=require('passport')
 
 var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 
-const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID2
-const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET1
+// const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID2
+// const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET1
 
-passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://bigbags.site/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
+// passport.use(new GoogleStrategy({
+//     clientID:     GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: "http://bigbags.site/google/callback",
+//     passReqToCallback   : true
+//   },
+//   function(request, accessToken, refreshToken, profile, done) {
      
-      return done(null, profile);
+//       return done(null, profile);
     
-  }
-));
-passport.serializeUser(function(user,done){
-    done(null,user)
-})
-passport.deserializeUser(function(user,done){
-    done(null,user)
-})
+//   }
+// ));
+// passport.serializeUser(function(user,done){
+//     done(null,user)
+// })
+// passport.deserializeUser(function(user,done){
+//     done(null,user)
+// })
 
